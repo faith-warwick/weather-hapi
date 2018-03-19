d55d079d51efd213b8e8f2b58b945447b234e0ad
Tyler Garlick <tjgarlick@gmail.com>
2018-03-19 15:27:29 -0600

    Fixes to hapi-boilerplate and now deployments

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+require('babel-polyfill')
 const ENV = process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 let Server
 if (ENV !== 'production') {
   require('babel-register')
-  require('babel-polyfill')
   Server = require('./src').default
-
 } else {
-  Server = require('./dist').default
+  Server = require('./build').default
 }
 
 const start = async () => {
